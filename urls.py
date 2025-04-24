4e835bc46482e5f365ff19488b0889b541a0e847
Daniel florez <florezx15@gmail.com>
2025-04-24 10:38:27 -0500

    Quickly access & menu navigation
    Config core APP for quick access.
    Implement class based view for users APP.
    user initial form configuration.
    menu navigation enable.

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import HomeView, UserCreationView
 
 app_name = 'users'
 urlpatterns = [
-    path(route='index',view=index,name='index')
+    path(route='home',view=HomeView.as_view(),name='home'),
+    path(route='user_creation',view=UserCreationView.as_view(),name='user_creation')
 ]
