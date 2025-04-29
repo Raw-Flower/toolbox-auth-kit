666413f6f5146641c75efc009725987866953c2b
Daniel florez <florezx15@gmail.com>
2025-04-29 14:33:33 -0500

    User creation form configuration
    Config user form.
    Add utils file for storage normal functions.
    Add render page to save code in form rendering.

--- models.py
+++ models.py
@@ -9,10 +9,10 @@ class StatusOptions(models.IntegerChoices):
     
 class UserProfile(models.Model):
     user = models.OneToOneField(verbose_name='User', to=User, on_delete=models.CASCADE)
-    location = models.CharField(verbose_name='Location')
-    contact_phone = models.CharField(verbose_name='Contact phone')
-    address = models.TextField(verbose_name='Address')
-    about_me = models.TextField(verbose_name='Bio')
+    location = models.CharField(verbose_name='Location', max_length=100)
+    contact_phone = models.CharField(verbose_name='Contact phone', max_length=100)
+    address = models.TextField(verbose_name='Address', max_length=100)
+    about_me = models.TextField(verbose_name='Bio', max_length=100)
     status = models.IntegerField(verbose_name='Status', choices=StatusOptions.choices)
     createtime = models.DateTimeField(verbose_name='Createtime', auto_now_add=True)
     updatetime = models.DateTimeField(verbose_name='Updatetime', auto_now=True)
