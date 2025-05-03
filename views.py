be31ca8cbce395a6034db5ccf26dcfa57fcdb4ba
Daniel florez <florezx15@gmail.com>
2025-05-02 19:00:25 -0500

    Loging & logout feature
    Login & logout are working fine.
    never_cache decorator works properly.
    Templates dirs have new structure.
    New auth_form.

--- views.py
+++ views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
-from django.contrib.auth.views import LoginView
 from django.contrib import messages
-from .forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, FormView
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login as auth_login
+from .forms import UserCreationForm, CustomAuthForm
 from .utils import split_forms, saveModelsInfo
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'users/index.html'
+    template_name = 'users/core/index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,7 +19,7 @@ class HomeView(TemplateView):
         return context
     
 class UserCreationView(FormView):
-    template_name = 'users/users_create.html'
+    template_name = 'users/core/users_create.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('users:home')
     
@@ -45,5 +48,42 @@ class UserCreationView(FormView):
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
     
-class LoginView(LoginView):
-    template_name = 'users/login.html'
+class CustomLoginView(LoginView):
+    template_name='users/auth/login.html'
+    form_class = CustomAuthForm
+    success_url = reverse_lazy('users:admin_home')
+    
+    def form_invalid(self, form):
+        messages.error(request=self.request, message="Your username and password didn't match. Please try again.")
+        return super().form_invalid(form)
+
+@method_decorator(never_cache, name='dispatch') 
+class CustomLogoutView(LoginRequiredMixin, LogoutView):
+    template_name='users/auth/logout.html'
+    success_url = reverse_lazy('users:home')
+    
+    def post(self, request, *args, **kwargs):
+        messages.success(request=self.request, message="You have been logout successfully.")
+        return super().post(request, *args, **kwargs)
+
+@method_decorator(never_cache, name='dispatch')  
+class AdminIndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/admin/index.html'
+    login_url = reverse_lazy('users:login')
+    redirect_field_name = 'next'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'admin_home'
+        return context
+
+@method_decorator(never_cache, name='dispatch')
+class AdminLogoutConfirmView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/admin/confirm_logout.html'
+    login_url = reverse_lazy('users:login')
+    redirect_field_name = 'next'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'admin_home'
+        return context
