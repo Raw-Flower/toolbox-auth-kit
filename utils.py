78292985543a55c84218aff48791fb8d82176e88
Daniel florez <florezx15@gmail.com>
2025-05-07 18:05:14 -0500

    Profile update feature
    Now current user can modify his own profile information(one form manage User and UserProfile model information)

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.forms.models import model_to_dict
 
 def split_forms(form):
     basic_form = [] 
@@ -23,23 +24,60 @@ def getProfileFields():
 def getMappingValuesByModel(clean_data,model_fields):
     return {k:v for k,v in clean_data if k in model_fields}
 
+def set_instance_values(instance,values2add):
+    for key, value in values2add.items():
+        setattr(instance, key, value)
+    return instance
+
+def build_init_data(user_instance):
+    profile_instance = model_to_dict(UserProfile.objects.get(user=user_instance))# Instance to python dict
+    profile_fields = getProfileFields()# Get all the fields names from UserProfile model
+    initial_data = {field:profile_instance[field] for field in profile_fields if field not in ['status','createtime','updatetime','id','user']}# Build init data
+    
+    # Add User fields
+    initial_data['first_name'] = user_instance.first_name
+    initial_data['last_name'] = user_instance.last_name
+    initial_data['email'] = user_instance.email
+    
+    # Return data
+    return initial_data
+
 def saveModelsInfo(cleaned_data):
     user_values = getMappingValuesByModel(cleaned_data.items(),getUserFields())
     profile_values = getMappingValuesByModel(cleaned_data.items(),getProfileFields())
     try:
-        #save User record
+        # Save User record
         userInstance = User(**user_values)
         userInstance.set_password(user_values.get('password'))
-        userInstance.is_staff = False #Not allow to login in admin panel
-        userInstance.is_superuser = False #Not superuser permissions/rights
+        userInstance.is_staff = False # Not allow to login in admin panel
+        userInstance.is_superuser = False # Not superuser permissions/rights
         userInstance.save()
 
-        #save Profile record
+        # Save Profile record
         profileInstance = UserProfile(**profile_values)
-        profileInstance.user = userInstance #Instance from user model
+        profileInstance.user = userInstance # Instance from user model
         profileInstance.save()
     except Exception as e:
         print(f'ERROR({type(e).__name__}): {e}')
         return False
     else:
         return True
+
+def updateModelsInfo(user_instance,cleaned_data):
+    user_values = getMappingValuesByModel(cleaned_data.items(),getUserFields())
+    profile_values = getMappingValuesByModel(cleaned_data.items(),getProfileFields())
+    
+    try:
+        # Set new values to instances
+        user_instance = set_instance_values(user_instance,user_values)
+        profile_instance = set_instance_values(UserProfile.objects.get(user=user_instance),profile_values)
+        
+        # Save updated information
+        user_instance.save()
+        profile_instance.save()
+    except Exception as e:
+        print(f'ERROR({type(e).__name__}): {e}')
+        return False
+    else:
+        return True
+    
--- views.py
+++ views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
-from .forms import UserCreationForm, CustomAuthForm, UserProfileUpdateForm, UserNewPasswordForm
-from .utils import split_forms, saveModelsInfo
+from .forms import UserCreationForm, CustomAuthForm, UserNewPasswordForm
+from .utils import split_forms, saveModelsInfo, build_init_data, updateModelsInfo
 from .mixins import NeverBrowserCache, SecureView
 
 # Create your views here.
@@ -86,7 +86,7 @@ class AdminLogoutConfirmView(SecureView, TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['active_page'] = 'admin_home'
+        context['active_page'] = 'confirm_logout'
         return context
     
 class AdminUserSettingsView(SecureView, TemplateView):
@@ -97,15 +97,40 @@ class AdminUserSettingsView(SecureView, TemplateView):
         context['active_page'] = 'user_settings'
         return context
     
-class AdminUserProfileView(SecureView, TemplateView):
+class AdminUserProfileView(SecureView, FormView):
     template_name = 'users/admin/user_profile.html'
-    form_class = UserProfileUpdateForm
+    form_class = UserCreationForm
     success_url = reverse_lazy('users:admin_home')
     
     def get_context_data(self, **kwargs):
+        init_data = build_init_data(self.request.user)
+        user_forms = split_forms(UserCreationForm(initial=init_data))
         context = super().get_context_data(**kwargs)
         context['active_page'] = 'user_settings'
+        context['profile_form'] = user_forms.get('profile_form')
         return context
+    
+    def form_valid(self, form):
+        result = updateModelsInfo(self.request.user, form.cleaned_data)
+        if result:
+            messages.success(request=self.request, message='You have been updated your profile information successfully.')
+            return super().form_valid(form)
+        messages.error(request=self.request, message='A critical error was found while saving the user data, please contact with the system administrator.')
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_invalid(self, form):
+        messages.error(request=self.request, message='Your data contains some errors. Please check and try again.')
+        return self.render_to_response(self.get_context_data(form=form))
+    
+    #Handle form validation 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        for field in list(form.fields):    
+            if field in ['username','password','password_repeat']:
+                del form.fields[field]        
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
     
 class AdminUserNewPasswordView(SecureView, PasswordChangeView):
     template_name = 'users/admin/user_new_password.html'
